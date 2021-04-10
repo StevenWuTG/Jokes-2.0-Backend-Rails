8506e019ac8d15a859b10514fdbc15eeaf1a5f15
StevenWuTG <stevenwu91@icloud.com>
2021-04-10 18:12:39 -0400

    added Faker gem and seeded some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "clearing seeds"
+
+Joke.destroy_all
+Gig.destroy_all
+Review.destroy_all
+Club.destroy_all
+User.destroy_all
+
+puts "faking some users"
+
+10.times do
+    User.create!(
+        name:Faker::FunnyName.two_word_name ,
+        email:Faker::Internet.email,
+        bio:Faker::Quote.famous_last_words 
+    )
+end
+
+puts "faking some clurbs"
+
+10.times do 
+    Club.create!(
+        name:Faker::Company.name,
+        required_likes:rand(1..50)
+    )
+end
+
+puts "faking some funnies"
+
+20.times do
+    Joke.create!(
+        # user_id: User.all.sample.id,
+        user_id: rand(1..10),
+        content:Faker::TvShows::FamilyGuy.quote
+    )
+end
+
+puts "faking some gigs"
+
+10.times do 
+    Gig.create!(
+        club_id: Club.all.sample.id,
+        user_id: User.all.sample.id,
+        
+    )
+end
+
+puts "faking some reviews"
+
+10.times do
+    Review.create!(
+        club_id: Club.all.sample.id,
+        rating: rand(1..5),
+        content:Faker::Restaurant.review
+
+    )
+end
